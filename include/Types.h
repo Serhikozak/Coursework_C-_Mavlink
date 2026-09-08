57c0f4ec549c011283b046cf0092a5f865d16348
Serhikozak <skozakov924@gmail.com>
2026-09-08 18:49:45 +0000

    added const

--- include/Types.h
+++ include/Types.h
@@ -8,6 +8,12 @@ constexpr int RC_MAX_FORWARD = 1100; // Максимальне прискоре�
 constexpr int RC_MIN_FORWARD = 1420; // Плавний рух
 constexpr int RC_IGNORE      = 0;    // Передати контроль оператору
 
+constexpr uint8_t  MAV_SYS_ID = 1;
+constexpr uint8_t  MAV_COMP_ID      = 1;
+constexpr uint16_t MAV_TARGET_PORT  = 14550; // UDP порт 14550
+constexpr int ACK_TIMEOUT_MS = 400; // Таймаут очікування ACK пакета
+constexpr int MAV_MAX_ATTEMPTS   = 5; // До 5 спроб скиду з очікуванням ACK
+
 // Стан польотної місії (FSM)
 enum class FlightState {
     MANUAL_OVERRIDE  = 0, // Керує оператор, автопілот шле 0
@@ -21,6 +27,12 @@ enum class FlightState {
 struct Coord {
     float x = 0.0f; // Північ (North) в метрах
     float y = 0.0f; // Схід (East) в метрах
+};
+
+//Географічні координати для MAVLink пакетів
+struct GPSPosition {
+    int32_t lat = 0; // Широта (Latitude) -> градуси * 1e7
+    int32_t lon = 0; // Довгота (Longitude) -> градуси * 1e7
 };
 
 // Телеметрія від польотника
